a1512dacd66764fc3bdc686187e5301fc19024ad
Aaron Jackson <ajackson@pobox.com>
2022-11-29 18:00:42 -0600

    Adding "nonsense" config.js as an example.

---
+++ webapp/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "cognito" : {
+        "region": "us-east-2",
+        "user_pool_id": "zoenqzyf",
+        "client_id": "19ze9819a3929a22",
+        "domain": "some-host.auth.us-east-2.amazoncognito.com",
+        "callback_url": "https://location/authorization",
+        "token_secret": "not a secret"
+    },
+    "dynamodb" : {
+        "region": "us-east-2"
+    },
+    "s3" : {
+        "region": "us-east-2"
+    }
+}
